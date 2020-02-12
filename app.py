de4a25c5a16895b704641502bf309a4d03e67eb2
AbMartha <martha.aboagye@duke.edu>
2020-02-12 18:03:57 -0500

    edited css and html for pages

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from flask import render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! This is our CS 316 project.  Prepare to be impressed.'
+def index():
+    return render_template('index.html')
     
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/')
+def database():
+    return render_template('database.html')
