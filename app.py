d6508dc0a91162a0fc9fd1ed58c2db9042aeb117
AbMartha <martha.aboagye@duke.edu>
2020-02-23 21:45:20 -0500

    created database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, url_for
 from flask import render_template
+from database import db
 
 
 app = Flask(__name__)
@@ -16,6 +17,15 @@ def welcome():
 @app.route('/database')
 def database():
     return render_template('database.html')
+
+@app.route('/select_singer')
+def select_singer():
+    artistname = request.args.get("Singer")
+    result = db.execute("SELECT * FROM artists WHERE artist_name = artistname")
+    for r in result:
+        print(r)
+
+return redirect(url_for('database'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
