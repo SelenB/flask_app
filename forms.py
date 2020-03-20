8cc49f8b70a32cfe6d95fd913241c36dff7aadfd
AbMartha <martha.aboagye@duke.edu>
2020-03-19 20:10:22 -0400

    added instruction to forms.py for what we are trying to do.

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+
+# create a dropdown list asking user what table they want to see
+# either top artists/top tracks/top albums. 
+#based on selection, open a new page that shows the table with
+#some data visualizations/world.
+
+#databasehtml is the page we will be using.
+
+#we also need to define stuff in app that allows forms to work. 
+
+
+
