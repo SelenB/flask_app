a2db748258397fba2f81abe78aaed18daff9a741
AbMartha <martha.aboagye@duke.edu>
2020-03-20 21:51:24 -0400

    added log in page. changed order of import so models and db arent self reference

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import models
 import forms
 
 app = Flask(__name__)
@@ -8,6 +7,7 @@ app.secret_key = 'cs316'
 app.config.from_object('config')
 db = SQLAlchemy(app, session_options={'autocommit': False})
 
+import models
 
 @app.route('/')
 def home():
@@ -18,12 +18,15 @@ def home():
 def welcome():
     return render_template('welcome.html')
 
-@app.route('/database')
-def database():
-    return render_template(
-        'database.html',
-
-        )
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/home')#when we figure out hoow to connect to spotify we can redirect here.
+    return render_template('login.html', form=form)
+   
 
 
 
