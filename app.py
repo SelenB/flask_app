f36f36099988eb492029f04a3354a9f1fe615370
AbMartha <martha.aboagye@duke.edu>
2020-03-21 13:15:27 -0400

    forms should work

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def welcome():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    form = forms.LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
